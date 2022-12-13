52fd4133d1e365214a8628fbff40611c427b76ba
Aleksey Klimov <alx111wrk@gmail.com>
2022-12-13 20:18:31 +0300

    3 ДЗ выполнено, 1 вер

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
-function compareArrays(arr1, arr2) {
-  
+function compareArrays(arr1, arr2) {  
+  if (JSON.stringify(arr1) === JSON.stringify(arr2)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users.filter(user => user.gender === gender).map(result => result.age).reduce((summ, userAge, index, array) => {
+    summ += userAge;
+    if (index === array.length - 1) {
+      return summ / array.length;
+    }
+    return summ;
+  }, 0);
 }
