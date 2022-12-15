aa9015004fdf08f7a9f7e5334d0efd42ba776cd5
Aleksey Klimov <alx111wrk@gmail.com>
2022-12-15 11:37:18 +0300

    3 дз доделано, убран лишний код

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,9 +1,5 @@
 function compareArrays(arr1, arr2) {  
-  if (JSON.stringify(arr1) === JSON.stringify(arr2)) {
-    return true;
-  } else {
-    return false;
-  }
+  return (JSON.stringify(arr1) === JSON.stringify(arr2));
 }
 
 function getUsersNamesInAgeRange(users, gender) {
