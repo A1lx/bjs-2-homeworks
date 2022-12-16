650df08f99545aac3a110f72621ee502ff135094
Aleksey Klimov <alx111wrk@gmail.com>
2022-12-16 16:47:17 +0300

    4 дз поправлено, убран лишний код

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -12,8 +12,6 @@ Student.prototype.setSubject = function (subjectName) {
 Student.prototype.addMarks = function (...marks) {
   if (this.marks !== undefined) {
     this.marks.push(...marks);
-  } else {
-    return;
   }
 }
 
