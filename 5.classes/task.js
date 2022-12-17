af2125193d61a4343257809d112e1239ee8419a6
Aleksey Klimov <alx111wrk@gmail.com>
2022-12-17 12:42:40 +0300

    5 дз, 2 задания

---
+++ 5.classes/task.js
@@ -0,0 +1,120 @@
+// 1 задача
+
+class PrintEditionItem {
+  constructor (name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;
+  }
+
+  fix() {
+    this.state *= 1.5;
+  }
+
+  set state (newState) {
+    if (newState < 0) {
+      this._state = 0;
+    } else if (newState > 100) {
+      this._state = 100;
+    } else {
+      this._state = newState;
+    }
+  }
+
+  get state () {
+    return this._state;
+  }
+}
+
+class Magazine extends PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.type = "magazine";
+  }
+}
+
+class Book extends PrintEditionItem {
+  constructor(author ,name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.author = author;
+    this.type = "book";
+  }
+}
+
+class NovelBook extends Book{
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "novel";
+  }
+}
+
+class FantasticBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "fantastic";
+  }
+}
+
+class DetectiveBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "detective";
+  }
+}
+
+// 2 задача
+
+class Library {
+  constructor(name, books) {
+    this.name = name;
+    this.books = [];
+  }
+
+  addBook(book) {
+    if (book.state > 30) {
+      this.books.push(book);
+    }
+  }
+  
+  findBookBy(type, value) {
+    for (let i = 0; i < this.books.length; i++) {
+      if (this.books[i][type] === value) {
+        return this.books[i];
+      }
+    }
+    return null;
+  }
+
+  giveBookByName(name) {
+    const bookToGive = this.findBookBy("name", name);
+    if (bookToGive) {
+      const bookIndex = this.books.indexOf(bookToGive);
+      this.books.splice(bookIndex, 1);
+      return bookToGive;
+    } else {
+      return null;
+    }
+  }
+}
+
+// //3 задача
+
+// class Student {
+//   constructor(name, marks) {
+//     this.name = name;
+//     this.marks = {};
+//   }
+
+//   addMark(mark, subject) {
+//     if (mark >= 2 && mark <= 5) {
+//       if (Object.keys(this).includes(subject) === false) {
+//         this.marks = [];
+//       }
+//     }
+
+//   }
+
+
+// }
